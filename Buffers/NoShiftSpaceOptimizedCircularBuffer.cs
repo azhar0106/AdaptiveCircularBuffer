b5dad56f1359a163b80ebbbfe736f2dbcadc9aea
Azhar Syed <azhar0106@hotmail.com>
2016-07-04 03:45:09 +0530

    Minor tweaks to NSSOCB.

--- Buffers/NoShiftSpaceOptimizedCircularBuffer.cs
+++ Buffers/NoShiftSpaceOptimizedCircularBuffer.cs
@@ -42,24 +42,7 @@ namespace Buffers
                 m_bufferSize += m_blockSize;
             }
         }
-        //protected override void AddBlock()
-        //{
-        //    if ((m_dataSize == 0) && (m_bufferSize == 0))
-        //    {
-        //        m_blockListReadHead = m_blockListWriteHead = m_blockList.AddFirst(CreateBlock());
-        //        m_bufferSize += m_blockSize;
-        //    }
-        //    else if ((m_blockWriteHead == m_blockSize - 1) && (m_bufferSize != m_maximumBufferSize))
-        //    {
-        //        var nextHead = m_blockListWriteHead.Next ?? m_blockList.First;
-        //        if (m_blockListReadHead == nextHead)
-        //        {
-        //            m_blockList.AddAfter(m_blockListWriteHead, CreateBlock());
-        //            m_bufferSize += m_blockSize;
-        //        }
-        //    }
-        //}
-
+        
         /// <summary>
         /// Remove as soon as an empty block is available.
         /// </summary>
@@ -70,7 +53,7 @@ namespace Buffers
                 return;
             }
 
-            if (m_bufferSize - m_dataSize == m_blockSize * 2)
+            if (m_bufferSize - m_dataSize >= m_blockSize * 2)
             {
                 var previousBlock = m_blockListReadHead.Previous ?? m_blockList.Last;
 
@@ -85,24 +68,7 @@ namespace Buffers
                 }
             }
         }
-        //protected override void RemoveBlock()
-        //{
-        //    if (m_dataSize == 0)
-        //    {
-        //        m_blockList.RemoveFirst();
-        //        m_bufferSize -= m_blockSize;
-        //    }
-        //    else if (m_blockReadHead == 0)
-        //    {
-        //        var prevHead = m_blockListReadHead.Previous ?? m_blockList.Last;
-        //        if (m_blockListWriteHead != prevHead)
-        //        {
-        //            m_blockList.Remove(prevHead);
-        //            m_bufferSize -= m_blockSize;
-        //        }
-        //    }
-        //}
-
+        
         protected override void CheckParameters(int blockSize, int maximumBufferSize)
         {
             if (blockSize <= 0)
